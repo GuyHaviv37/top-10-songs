9a18d8778fb4447dd46861637799a23ae803648a
Guy Haviv <theguyhaviv@gmail.com>
2024-12-06 16:33:22 +0200

    update vite.config base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/top-10-songs/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
